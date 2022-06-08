03736c86f1b70605e948596c1b972beccf631c2f
Fabio Moro <fabio.moro2000@gmail.com>
2022-06-08 13:57:42 +0200

    aggiunta ricerca per genere

---
+++ src/components/common/SelectBar.vue
@@ -0,0 +1,32 @@
+<template>
+    <form>
+        <select value="all" name="genre" id="genre" v-model="select.selectedText">
+            <option value="all">All</option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
+        </select>
+    </form>
+</template>
+
+<script>
+import select from '../../shared/select.js';
+
+export default {
+    name: 'SelectBar',
+    data() {
+        return {
+            select,
+        };
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+select {
+    margin: 20px;
+}
+
+</style>
--- src/components/sections/AlbumSection.vue
+++ src/components/sections/AlbumSection.vue
@@ -1,8 +1,9 @@
 <template>
     <section>
         <div class="container">
+            <SelectBar/>
             <div class="row">
-                <CardAlbum class="col my-1" v-for="album in albums" :key="album.title" :album="album"/>
+                <CardAlbum class="col my-1" v-for="album in genreFiltered" :key="album.title" :album="album"/>
             </div>
         </div>
     </section>
@@ -10,17 +11,21 @@
 
 <script>
     import axios from 'axios';
+    import SelectBar from '../common/SelectBar.vue'
     import CardAlbum from '../common/CardAlbum.vue';
+    import select from '../../shared/select';
 
     export default{
         name: 'AlbumSection',
         data() {
             return {
+                select,
                 albums: [],
             };
         },
 
         components: {
+            SelectBar,
             CardAlbum,
         },
 
@@ -34,6 +39,19 @@
                 // handle error
                 console.log(error);
             });
+        },
+
+        computed: {
+            genreFiltered() {
+                if(this.select.selectedText.toLowerCase()=="all") {
+                    return this.albums;
+                } else 
+                {
+                    return this.albums.filter((elm)=> {
+                        return elm.genre.toLowerCase()==(this.select.selectedText.toLowerCase());
+                    })
+                }
+            }
         }
     }
 </script>
@@ -43,5 +61,9 @@
 .container {
     padding: 50px;
     margin: auto;
+}
+
+section {
+    min-height: calc(100vh - 60px);
 }
 </style>
